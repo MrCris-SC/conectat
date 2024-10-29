d6667180983e52e6dade5a764734329a5d607c3c
MrCris-SC <cristino_1401@hotmail.com>
2024-10-28 21:05:11 -0600

    reposive admin pages

--- resources/views/agregar-paquete.blade.php
+++ resources/views/agregar-paquete.blade.php
@@ -7,108 +7,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
-
     <title>Agregar Paquete de Internet</title>
-    <link href="{{ asset('css/agregar-paq.css') }}" rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js"></script>
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
-<div id="wrapper">
+    <div id="wrapper">
         <!-- Sidebar -->
-         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <!-- Sidebar - Brand -->
              <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 <div class="sidebar-brand-icon rotate-n-15">
                      <i class="fas fa-globe"></i>
                     </div>
                     <div class="sidebar-brand-text mx-3">Admin Panel</div>
-                </a>
+             </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
 
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Gestion de paquetes:
-    </div>
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Gestion de paquetes:
+            </div>
+           
+            <!-- Nav Item - Paquetes -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{url ('/')}}">
+                    <i class="fas fa-fw fa-box"></i>
+                    <span>Gestión de Paquetes</span></a>
+            </li>
 
-    <!-- Nav Item - Paquetes -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{url ('/')}}">
-            <i class="fas fa-fw fa-box"></i>
-            <span>Gestión de Paquetes</span></a>
-    </li>
+            <!-- Nav Item - Clientes -->
+            <li class="nav-item">
+                <a class="nav-link" href="clienteRegistrados">
+                    <i class="fas fa-fw fa-users"></i>
+                    <span>Gestión de Clientes</span></a>
+            </li>
 
-    <!-- Nav Item - Clientes -->
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/clienteRegistrados') }}">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Gestión de Clientes</span></a>
-    </li>
+            <!-- Nav Item - Clientes -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/indexAdmin') }}">
+                    <i class="fas fa-fw fa-users"></i>
+                    <span>Gestión de Adminisreadores</span></a>
+            </li>
 
-      <!-- Nav Item - Clientes -->
-      <li class="nav-item">
-        <a class="nav-link" href="{{ url('/indexAdmin') }}">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Gestión de Adminisreadores</span></a>
-    </li>
-    <!-- Nav Item - Facturación -->
-    <li class="nav-item">
-        <a class="nav-link" href="facturacion.html">
-            <i class="fas fa-fw fa-file-invoice-dollar"></i>
-            <span>Facturación</span></a>
-    </li>
+            <!-- Nav Item - Facturación -->
+            <li class="nav-item">
+                <a class="nav-link" href="facturacion.html">
+                    <i class="fas fa-fw fa-file-invoice-dollar"></i>
+                    <span>Facturación</span></a>
+            </li>
 
-    <!-- Nav Item - Reportes -->
-    <li class="nav-item">
-        <a class="nav-link" href="reportes.html">
-            <i class="fas fa-fw fa-chart-line"></i>
-            <span>Reportes</span></a>
-    </li>
+            <!-- Nav Item - Reportes -->
+            <li class="nav-item">
+                <a class="nav-link" href="reportes.html">
+                    <i class="fas fa-fw fa-chart-line"></i>
+                    <span>Reportes</span></a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Configuración
-    </div>
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Configuración
+            </div>
 
-    <!-- Nav Item - Ajustes -->
-    <li class="nav-item">
-        <a class="nav-link" href="ajustes.html">
-            <i class="fas fa-fw fa-cogs"></i>
-            <span>Ajustes del Sistema</span></a>
-    </li>
+            <!-- Nav Item - Ajustes -->
+            <li class="nav-item">
+                <a class="nav-link" href="ajustes.html">
+                    <i class="fas fa-fw fa-cogs"></i>
+                    <span>Ajustes del Sistema</span></a>
+            </li>
 
-    <!-- Nav Item - Ayuda -->
-    <li class="nav-item">
-        <a class="nav-link" href="ayuda.html">
-            <i class="fas fa-fw fa-question-circle"></i>
-            <span>Ayuda</span></a>
-    </li>
+            <!-- Nav Item - Ayuda -->
+            <li class="nav-item">
+                <a class="nav-link" href="ayuda.html">
+                    <i class="fas fa-fw fa-question-circle"></i>
+                    <span>Ayuda</span></a>
+            </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
 
         </ul>
         <!-- End of Sidebar -->
@@ -122,7 +126,11 @@
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
                 
-                   
+                   <!-- Sidebar Toggle (Topbar) -->
+                    
+                   <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
